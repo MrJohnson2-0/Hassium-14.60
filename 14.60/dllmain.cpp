a09f8342d93309f9288019d2dca8efbfa218f7cd
MrJohnson2-0 <unepicgamed@gmail.com>
2024-05-02 19:22:58 -0400

    Base Code!

--- 14.60/dllmain.cpp
+++ 14.60/dllmain.cpp
@@ -1,10 +1,32 @@
 #include "ue.h"
 
+DWORD Main(LPVOID)
+{
+    if (Debugging)
+    {
+        AllocConsole();
+        FILE* sFile;
+        freopen_s(&sFile, "CONOUT$", "w", stdout);
+        SetConsoleTitleA("Hassium 14.60 || Loading Funcs...");
+        std::cout << "BaseAddr: {}" + GetBaseAddr();
+        if (GetEngine == UFortEngine::GetDefaultObj)
+        {
+            std::cout << "FortEngine_0 is Default Object of class UFortEngine";
+        }
+        else {
+            std::cout << "UFortEngine DefaultObject: ";
+        }
+    }
+    GetKKSL()->ExecuteConsoleCommand(GetWorld(), L"open Apollo_Terrain", nullptr);
+    return 0;
+}
+
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
     switch (reason)
     {
     case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, Main, 0, 0, 0);
         break;
     }
     return TRUE;
--- 14.60/ue.h
+++ 14.60/ue.h
@@ -15,3 +15,23 @@ UWorld* GetWorld()
 	auto World = GetEngine()->GameViewport->World;
 	return World;
 }
+
+uintptr_t GetBaseAddr()
+{
+	auto BaseAddr = __int64(GetModuleHandleA(0));
+	return BaseAddr;
+}
+
+UKismetStringLibrary* GetKSL()
+{
+	auto Default = UKismetStringLibrary::GetDefaultObj();
+	return Default;
+}
+
+UKismetSystemLibrary* GetKKSL()
+{
+	auto Default = UKismetSystemLibrary::GetDefaultObj();
+	return Default;
+}
+
+bool Debugging = true;
